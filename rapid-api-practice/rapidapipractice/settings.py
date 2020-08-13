4c007f57cc4b2ce09c7ca8c24a5aff387ee76ec1
MWeberLambdaweb19 <mack.webb37@gmail.com>
2020-08-13 16:40:30 -0700

    Added urls, updated settings with pagination

--- rapid-api-practice/rapidapipractice/settings.py
+++ rapid-api-practice/rapidapipractice/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination', 'PAGE_SIZE': 10
 }
 
 MIDDLEWARE = [
--- rapid-api-practice/rapidapipractice/urls.py
+++ rapid-api-practice/rapidapipractice/urls.py
@@ -23,5 +23,7 @@ router.register(r'users', views.UserViewset)
 router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
